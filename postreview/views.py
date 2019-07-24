e3ef55184bf957a83d8a4a95cf72f292bfaf7088
Ananya Malik <ananyamalik1999@gmail.com>
2019-07-24 19:19:47 +0530

    Added Pagination for Event list

--- postreview/views.py
+++ postreview/views.py
@@ -30,12 +30,17 @@ def post_update_view(request, id=id):
 
 
 def list_reviews(request):
-    queryset = Review.objects.all()  # list of objects
+    queryset_list = Review.objects.all()  # list of objects
+    query = request.GET.get("q",None)
+    if query:
+        queryset_list =  queryset_list.filer(Q(first_name__icontains=query) | Q(last_name__icontains=query) | Q(photograph__icontains=query) | Q(designation__icontains=query) | Q(company__icontains=query) | Q(comments__icontains=query)).distinct()
+    paginator = Paginator(queryset_list, 10)
+    page = request.GET.get('page')
+    object_list = paginator.get_page(page)
     context = {
-        "object_list": queryset
+        "object_list": object_list
     }
     return render(request, "postreview/review_list.html", context)
-
 
 def review_detail_view(request, id):
     obj = get_object_or_404(Review, id=id)
