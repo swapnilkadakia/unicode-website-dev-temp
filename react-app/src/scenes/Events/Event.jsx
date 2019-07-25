c7673cde5fdf66e5cad849e51908f86fb9c0544c
swapnil kadakia <swapnilkadakia@gmail.com>
2019-07-25 11:55:02 +0530

    Events page

--- react-app/src/scenes/Events/Event.jsx
+++ react-app/src/scenes/Events/Event.jsx
@@ -1,45 +1,42 @@
-import React, { Component } from 'react';
-import Header from '../components/header';
-import Footer from '../components/footer';
-import Card from '../components/Events/EventCard';
-import { Grid} from '@material-ui/core';
+import React, { Component } from "react";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+//import Card from '../components/Events/EventCard';
+import Card from "./components/EventCard";
+import { Grid } from "@material-ui/core";
 
 class App extends Component {
-
-  styles={
-    grid:{
-        marginTop: 117,
-        marginBottom: 161,
-        // padding: 0,
-        width: "100%"
+  styles = {
+    grid: {
+      marginTop: 117,
+      marginBottom: 161,
+      // padding: 0,
+      width: "100%"
     },
-    background:{
+    background: {
       background: "#F8F8F8",
       marginTop: 114
     },
-    width:{
+    width: {
       width: 700
     }
-  }
+  };
   render() {
     return (
-    <div style={this.styles.background}>
-      <Header />
-          <Grid 
-            container 
-            spacing={24}
-            direction="row"
-            justify="center"
-            style={this.styles.grid}
-          >
-
-            <Grid item xs={9}>
-                <Card />
-            </Grid>
-
+      <div style={this.styles.background}>
+        <Grid
+          container
+          spacing={24}
+          direction="row"
+          justify="center"
+          style={this.styles.grid}
+        >
+          <Grid item xs={9}>
+            <Card />
           </Grid>
-    <Footer />
-  </div>
+        </Grid>
+        <Footer />
+      </div>
     );
   }
 }
